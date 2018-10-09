df9c31e4679a620f4655c59d8b33e2ceaf339916
Andrew Wheeler <awheeler@starsinc.com>
2018-10-09 15:04:23 -0700

    Changed view engine to pug

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ var path = require('path');
 var router = express.Router();
 var dataset = [];	
 
-
+app.engine('pug', require('pug').__express);
 app.set('views', path.join(__dirname, 'views'));  
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(router);  
 app.use(express.static(path.join(__dirname, 'public')));
