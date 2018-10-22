1be3d7ee15654564b35c97cd894b6ee3af7b3b78
Andrew Wheeler <awheeler@starsinc.com>
2018-10-22 13:42:15 -0700

    Changed default route as can not figure how to get pug the way I want

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ var fs = require('fs');
 
 
 var router = express.Router();
-var dataset = [];	
+var menu_data = [];	
+var menu = [];
 
 app.engine('pug', require('pug').__express);
 app.set('views', path.join(__dirname, 'views'));  
@@ -30,11 +31,90 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 router.get('/', getMenuList, function (req, res, next) {
-    res.render('menu_items', {
+
+  
+     var item = [];
+     for(var row of menu_data ){
+       if(menu.length == 0) {
+         var newItem = {Date: row.MenuDate, Items: item};
+                  
+         menu.push(newItem);
+         
+
+        
+       }
+       if(row.MenuDate != menu[menu.length -1].Date) {
+        
+        item = [];
+         
+         var newItem = {Date: row.MenuDate, Items: item};
+         menu.push(newItem);
+         
+         
+       }
+       var newDish = {Dish: row.Item};
+       
+       menu[menu.length -1].Items.push(row.Item);
+      /* var i = 0;
+       while(i < menu[menu.length -1].Items.length) {
+         console.log( menu[menu.length -1].Items[i]);
+         i++;
+       }
+       */
+
+       
+       
+
+       
+       
+     }
+     
+     fs.writeFileSync('./views/message.html', '<!DOCTYPE html><html><head><title>The Weekly Menu</title><link rel="stylesheet" href="/stylesheets/style.css"></head><body>', 'utf8', function(err){
+      if (err) throw err;
+     });
+     fs.appendFileSync('./views/message.html', '<h1>Why the drama</h1>', function(err){
+       if (err) throw err;
+     });
+    for(index = 0; index < menu.length ; index++ ) {
+        //console.log(menu[index].Date);
+        if(index ===0){
+          fs.appendFileSync('./views/message.html', '\r\n<ul>', function(err){
+            if(err) throw err;
+          });
+        }
+        fs.appendFileSync('./views/message.html', '\r\n<li>'  + menu[index].Date + '</li>', function(err){
+          if(err) throw err;
+        });
+
+            for(newindex = 0; newindex < menu[index].Items.length; newindex++) {
+                //console.log(menu[index].Items[newindex])
+                if(newindex === 0 ){
+                fs.appendFileSync('./views/message.html','\r\n<ul>', function(err){
+                  if(err) throw err;
+                  });
+                }
+                fs.appendFileSync('./views/message.html', '\r\n<li> ' + menu[index].Items[newindex] + '</li>', function(err){
+                  if(err) throw err;
+                  });
+              }
+              fs.appendFileSync('./views/message.html', '\r\n</ul', function(err){
+                if(err) throw err;
+              });
+      }  // end of outer loop
+
+      fs.appendFileSync('./views/message.html', '\r\n</ul>\r\n</body>\r\n</html>', function(err){
+        if(err) throw err;
+      });
+                
+   /* res.render('menu_items', {
       title: 'The Weekly Menu',
       message: 'The List',
-      dataset
-  });
+      menu_data,
+      menu
+     
+  }); */
+
+   res.sendfile('./views/message.html');
 });
 
 router.get('/index', function(req, res) {
@@ -42,14 +122,13 @@ router.get('/index', function(req, res) {
 });
 
 router.post('/update',submitMenuItem, function (req, res) {  
-	  var menu_date = req.body.menuDate;
+	  var menu_data = req.body.menuDate;
       var description = req.body.theItem;
-      //console.log(menu_date);
-      //console.log(description);
+      
 	  
     res.render('update', {
         title: 'All Quiet on the Western Front',
-        menuDate: menu_date,
+        menuDate: menu_data,
         MenuItem: description 
       
           
@@ -89,16 +168,18 @@ function getMenuList( req, res, next) {
               executeStatement();
          }
     });
-    function MenuItem(Menu_Date, Name, Display)
+   /* function MenuItem(Menu_Date, Name, Display)
     {
-    	this.Menu_Date = Menu_Date;
+    	this.Menu_Data = Menu_Date;
     	this.Name = Name;
-        this.Display = Display;
+      this.Display = Display;
+    
    
-    }
+    }  */
+    
     function executeStatement() {
 
-         var sql = "select dt_ofItem ,dish_name  FROM LA_COUNTY.dbo.mn_items"; 
+         var sql = "select dt_ofItem ,dish_name  FROM LA_COUNTY.dbo.mn_items order by dt_ofItem"; 
     	
          var Request = require('tedious').Request;
          request = new Request(sql, function (err, rowCount) {
@@ -109,7 +190,7 @@ function getMenuList( req, res, next) {
            		   callback(null, false);
            	   }
            	   else {
-           		fulfill(dataset);
+           		fulfill(menu_data);
            	   }
               }
          });
@@ -129,10 +210,16 @@ function getMenuList( req, res, next) {
             	  if(column.metadata.colName==="dish_name") {
                       //console.log(column.value);
                       //console.log('WTF');
-            		  var item = new MenuItem(Menu_Date, column.value, Menu_Date + ": " + column.value);
-            		  dataset.push(item);
-                      //console.log(item.Menu_Date, item.Name);
-            		  item = null;
+                  //var item = new MenuItem(Menu_Date, column.value, Menu_Date + ": " + column.value);
+                  var menu_stuff = {
+                    MenuDate: Menu_Date,
+                    Item: column.value
+                    
+                };
+                  menu_data.push(menu_stuff);
+            		  //menu_data.push("Date" Menu_Date ,"Item" column.value );
+                      
+            		  //item = null;
             	  }
               });
               
@@ -147,9 +234,15 @@ function getMenuList( req, res, next) {
          request.on('doneProc', function (rowCount, more, returnStatus, rows) {
         	  next(null, rows);
               connection.close()
+             
               
               
-              dataset = [];
+              
+    
+             // console.log(menu_data[0].MenuDate);
+            
+
+              menu_data = [];
 
          });
 
@@ -194,7 +287,7 @@ var config = JSON.parse(fs.readFileSync('./config/config.json', 'utf8'));
             callback (null, false);
       }
       else {
-          fulfill(dataset);
+          fulfill(menu_date);
       }
     }
     });
